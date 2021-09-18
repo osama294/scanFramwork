5510d597419bf7021dcd1c95ea76f8d61542baa9
OSAMA SHIRAZ <osamashiraz60@gmail.com>
2021-09-18 18:24:50 +0500

    Initla Commit

---
+++ ScanCocoaPod/ScanCocoaPod/ScanCocoaPod.h
@@ -0,0 +1,18 @@
+//
+//  ScanCocoaPod.h
+//  ScanCocoaPod
+//
+//  Created by Osama's Macbook on 18/09/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ScanCocoaPod.
+FOUNDATION_EXPORT double ScanCocoaPodVersionNumber;
+
+//! Project version string for ScanCocoaPod.
+FOUNDATION_EXPORT const unsigned char ScanCocoaPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ScanCocoaPod/PublicHeader.h>
+
+
---
+++ ScanCocoaPod/ScanCocoaPod/Service.swift
@@ -0,0 +1,17 @@
+//
+//  Service.swift
+//  ScanCocoaPod
+//
+//  Created by Osama's Macbook on 18/09/2021.
+//
+
+import Foundation
+
+public class Service {
+    private init(){
+        
+    }
+    public static func doSomething() -> String {
+        return "Did Some Stuff"
+    }
+}
